7a58bdd2cb06a26747778245cc1be00e37753041
Seum <mostofaseum8@gmail.com>
2026-04-28 21:37:24 +0600

    Initial Commit

---
+++ EventManagement.Web/Controllers/EventTypeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EventManagement.Web.Controllers
+{
+    public class EventTypeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Event Type Index");
+        }
+    }
+}
